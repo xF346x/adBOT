7dccd98bc92838fd7fd1b87aa29ed81c792a3171
DD <68047131+xF346x@users.noreply.github.com>
2020-08-30 02:57:15 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,11 +1,12 @@
 <?php
 
 /* Config adBOT.cc */
-// USER AGENT DI REKOMENDASIKAN DIGANTI DENGAN USER-AGENT MASING MASING !!!
-$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.50";
+
+$key = "7sKXmRTDPxBAoHQ";
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.61";
 $ssidzec = "";
 $ssidltc = "";
 $ssiddoge = "";
 $ssidbch = "";
+$ssideth = "";
 $valsurf = "SAsurf=SAsurfVal";
-$key = "7sKXmRTDPxBAoHQ";
