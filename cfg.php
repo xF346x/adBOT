5e0c2a85f34273a2925c277140d2417598cafdc9
DD <68047131+xF346x@users.noreply.github.com>
2020-08-30 03:28:43 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,8 +1,6 @@
 <?php
-
-/* Config adBOT.cc */
-
-$key = "7sKXmRTDPxBAoHQ"; // ==> https://bit.ly/31EV62r <== Ambil Keys Disini
+/* Ambil Keys Disini ==> https://bit.ly/31EV62r */
+$key = "7sKXmRTDPxBAoHQ"; 
 $useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.61";
 $ssidzec = "";
 $ssidltc = "";
