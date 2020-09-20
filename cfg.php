0f9d6bf4474459e5bafb2755598b580604818eab
mijazz <71597772+mijazz@users.noreply.github.com>
2020-09-20 16:20:35 +0200

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,10 +1,11 @@
 <?php
-/* Ambil Keys Disini ==> https://bit.ly/31EV62r */
-$key = "7sKXmRTDPxBAoHQ"; 
-$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.61";
-$ssidzec = "";
-$ssidltc = "";
-$ssiddoge = "";
-$ssidbch = "";
-$ssideth = "";
+/* Ambil Keys Disini ==> http://keys.ggloantb.host/
+$key = "FRi30=87*/Ht3Zxs"; 
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36";
+$ssidzec = "hheuob814lsftvvfv6fkktt1h6";
+$ssidltc = "5o608bfidn1gdn8k130fdes2s0";
+$ssiddoge = "63i4tvbaangt7dp1kkbeqgd341";
+$ssidbch = "5at0uqap80ic8bqs6obnlfm5l2";
+$ssideth = "081v26194g6kb3k7c0uvi26es2";
 $valsurf = "SAsurf=SAsurfVal";
+
