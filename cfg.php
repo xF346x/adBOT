6a31d908ba3c4edf1d3f00ac1ea88db511c33c76
DD <68047131+xF346x@users.noreply.github.com>
2020-09-22 00:59:19 +0700

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -1,10 +1,15 @@
 <?php
-/* Ambil Keys Disini ==> https://bit.ly/31EV62r */
-$key = "7sKXmRTDPxBAoHQ"; 
-$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36 Edg/84.0.522.61";
+
+/* Config adBOT.cc */
+
+$key = "";
+$valsurf = "SAsurf=SAsurfVal";
+$useragent = "";
 $ssidzec = "";
 $ssidltc = "";
-$ssiddoge = "";
 $ssidbch = "";
 $ssideth = "";
-$valsurf = "SAsurf=SAsurfVal";
+$ssidxrp = "";
+$ssidbtc = "";
+$ssiddash = "";
+$ssiddoge = "";
